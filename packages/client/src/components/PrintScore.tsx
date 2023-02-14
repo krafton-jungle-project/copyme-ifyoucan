5fb0f0fbe9217a7aa187ee2cbf7b4b5564972138
조제희 <chojehee@chojehee-MacBookAir.local>
2023-02-14 22:04:40 +0900

    feat: 유사도 점수 컴포넌트 추가

--- packages/client/src/components/PrintScore.tsx
+++ packages/client/src/components/PrintScore.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from 'react';
+import { comparePoses } from '../utils/pose-similarity';
+import * as poseDetection from '@tensorflow-models/pose-detection';
+import * as movenet from '../utils/tfjs-movenet';
+import styled from 'styled-components';
+
+const Score = styled.div`
+  width: 150px;
+  height: 150px;
+  font-size: 100px;
+  text-align: center;
+`;
+
 function PrintScore() {
-  return <></>;
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    let pose: poseDetection.Pose;
+    async function b() {
+      const poses = await movenet.detector.estimatePoses(movenet.camera.video);
+      if (poses && poses.length > 0) {
+        pose = poses[0];
+        let s = Math.ceil(100 - 100 * comparePoses(pose, pose));
+        setScore(s > 0 ? s : 0);
+      }
+    }
+    setInterval(() => {
+      b();
+    }, 1000);
+  }, []);
+  return (
+    <>
+      <Score>{score}</Score>
+    </>
+  );
 }
 
 export default PrintScore;
