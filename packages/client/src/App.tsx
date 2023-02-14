51d96e138fc8fdcdc14fe37911581e37804238c0
jungle-taeuk <gotlou8317@gmail.com>
2023-02-14 15:14:53 +0900

    style: 파일 자동 정렬

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Lobby from './pages/lobby';
 import Room from './pages/room';
 
-
 export default function App() {
   return (
     <div className="App">
