02dcfa193126d553712f0a193d6bdb3c5d15a86a
Choinbong <cho5048@dgu.ac.kr>
2023-02-15 23:18:31 +0900

    feat: pose와 imgSrc(base64) 받아서 소켓 emit

--- packages/client/src/utils/capture-pose.ts
+++ packages/client/src/utils/capture-pose.ts
@@ -1,11 +1,23 @@
+import type * as poseDetection from '@tensorflow-models/pose-detection';
+import type { Socket } from 'socket.io-client';
 import html2canvas from 'html2canvas';
 import getPose from './get-pose';
+
+// 이미지와 keypoints 데이터 보내는 이벤트는 캡처할 때 밖에 없으므로 여기서 소켓 통신까지 함
+async function emitDataToDefender(imgSrc: string, socket: Socket) {
+  const pose = await getPose();
+  if (pose !== undefined) {
+    const data: { pose: poseDetection.Pose; imgSrc: string } = { pose, imgSrc };
+    socket.emit('image', data);
+  }
+}
 
 async function Capture(
   toDraw: HTMLCanvasElement,
   toCapture: HTMLVideoElement,
   canvasWidth: number,
   canvasHeight: number,
+  socket: Socket,
 ) {
   if (toDraw !== null && toCapture !== null) {
     // canvas 크기 설정
@@ -16,14 +28,11 @@ async function Capture(
     await html2canvas(toCapture).then(async (canvas) => {
       const ctx = toDraw?.getContext('2d');
       ctx?.drawImage(canvas, 0, 0, canvasWidth, canvasHeight);
+      let imgSrc: string = canvas.toDataURL();
+      console.log(imgSrc);
+      emitDataToDefender(imgSrc, socket);
     });
   }
-
-  // 캡처 시 포즈 리턴해서 서버로 보내줌
-  // TODO: socket 통해서 보내줘야 할 것 : 이미지, 포즈(완) 키포인트
-  let pose = await getPose();
-  console.log('capture:', pose);
-  return pose;
 }
 
 export default Capture;
