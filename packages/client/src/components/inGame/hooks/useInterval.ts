bb28f3c6cd85beb17ca2ebae10128623efbe5aa4
cityzenKIM <xowns1016@gmail.com>
2023-02-20 22:37:43 +0900

    faet: 포즈 유사도 계산 기능 구현

---
+++ packages/client/src/components/inGame/hooks/useInterval.ts
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from 'react';
+
+function useInterval(callback: () => void, delay: number | null) {
+  const savedCallback = useRef<any>();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      if (savedCallback.current) {
+        savedCallback.current();
+      }
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+export { useInterval };
