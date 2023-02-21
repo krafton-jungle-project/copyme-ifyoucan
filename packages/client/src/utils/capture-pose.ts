4660103b4e898b30c2381a51dc8a120ae524b20e
Choinbong <cho5048@dgu.ac.kr>
2023-02-21 17:35:49 +0900

    feat: 공격 수비 사진 데이터가 모두 있을 때 소켓으로 전송

--- packages/client/src/utils/capture-pose.ts
+++ packages/client/src/utils/capture-pose.ts
@@ -1,27 +1,40 @@
 import type * as poseDetection from '@tensorflow-models/pose-detection';
 import type { Socket } from 'socket.io-client';
 import html2canvas from 'html2canvas';
+import { useClientSocket } from '../module/client-socket';
+
+let data: [string, string] = ['', ''];
 
 // 이미지와 keypoints 데이터 보내는 이벤트는 캡처할 때 밖에 없으므로 여기서 소켓 통신까지 함
-async function emitDataToDefender(imgSrc: string, socket: Socket) {
-  socket.emit('image');
+async function emitDataToDefender(imgSrc: string, flag: number) {
+  const { socket } = useClientSocket();
+  data[flag] = imgSrc;
+  if (data[0] !== '' && data[1] !== '') {
+    socket.emit('image', data);
+    data = ['', ''];
+  }
 }
+
+/**
+ *
+ * @param flag
+ * flag === 0 : 공격자 이미지
+ * flag === 1 : 수비자 이미지
+ */
 
 export async function capturePose(
   toCapture: HTMLVideoElement,
   toDraw: HTMLCanvasElement,
-  socket?: Socket,
+  flag: number,
 ) {
   if (toCapture !== null) {
     // 캡처할 html 요소를 넣으면 return 값이 'HTMLCanvasElement'로 나옴
     await html2canvas(toCapture).then(async (canvas: HTMLCanvasElement) => {
       const ctx = toDraw.getContext('2d') as CanvasRenderingContext2D;
       ctx.drawImage(canvas, 0, 0, 640, 480);
-      let imgSrc: string = canvas.toDataURL();
 
-      if (socket) {
-        emitDataToDefender(imgSrc, socket);
-      }
+      let imgSrc: string = canvas.toDataURL('image/webp', 0.5);
+      emitDataToDefender(imgSrc, flag);
     });
   }
 }
