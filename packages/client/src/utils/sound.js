cec5516da8f1ad3a94279b9a8e1c68bcef61f0b7
jungle-taeuk <gotlou8317@gmail.com>
2023-02-22 20:10:03 +0900

    feat: background music 추가(미적용)

--- packages/client/src/utils/sound.js
+++ packages/client/src/utils/sound.js
@@ -3,12 +3,13 @@ import gamemusic from '../assets/sounds/game-music.mp3';
 import bell from '../assets/sounds/bell.mp3';
 import gunreload from '../assets/sounds/gun-reload.mp3';
 import cameraclick from '../assets/sounds/camera-click.mp3';
+import backgroundmusic from '../assets/sounds/background-music.mp3';
 
 const countDown = new Audio(countdown);
 const gameMusic = new Audio(gamemusic);
-gameMusic.volume = 0.5;
 const Bell = new Audio(bell);
 const gunReload = new Audio(gunreload);
 const cameraClick = new Audio(cameraclick);
+const backgroundMusic = new Audio(backgroundmusic);
 
-export { countDown, gameMusic, Bell, gunReload, cameraClick };
+export { countDown, gameMusic, Bell, gunReload, cameraClick, backgroundMusic };
