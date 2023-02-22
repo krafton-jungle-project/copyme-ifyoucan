8dc779b65df1d46b2ceb9279cd40b6e369aa756b
jungle-taeuk <gotlou8317@gmail.com>
2023-02-23 07:25:48 +0900

    design: versus 애니메이션 보완

--- packages/client/src/components/inGame/game/Versus.tsx
+++ packages/client/src/components/inGame/game/Versus.tsx
@@ -1,5 +1,5 @@
 import { useAtomValue } from 'jotai';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { isStartAtom } from '../InGame';
 
 const VS = styled.div<{ isStart: boolean }>`
@@ -7,23 +7,36 @@ const VS = styled.div<{ isStart: boolean }>`
   display: flex;
   justify-content: center;
   align-items: center;
-  top: ${(props) => (props.isStart ? '30%' : '50%')};
-  left: ${(props) => (props.isStart ? '40%' : '50%')};
-  width: ${(props) => (props.isStart ? '20%' : '0%')};
-  height: ${(props) => (props.isStart ? '40%' : '0%')};
-  font-size: 7vw;
-  font-weight: 1000;
 
-  border-radius: 2rem;
-  padding: 0.4em;
+  ${(props) =>
+    props.isStart &&
+    css`
+      top: 30%;
+      left: 40%;
+      width: 20%;
+      height: 40%;
+      font-size: 7vw;
+      font-weight: 1000;
+    `}
+
+  ${(props) =>
+    !props.isStart &&
+    css`
+      top: 50%;
+      left: 50%;
+      width: 0%;
+      height: 0%;
+      font-size: 0;
+      font-weight: 0;
+    `}
+    
   color: #fff;
   text-shadow: /* White glow */ 0 0 5px #fff, 0 0 20px #fff, 0 0 50px #fff,
     /* Green glow */ 0 0 42px #bc13fe, 0 0 120px #bc13fe, 0 0 92px #bc13fe, 0 0 102px #bc13fe,
     0 0 151px #bc13fe;
 
-  transition-property: top, left, width, height;
+  transition: 0.5s;
   transition-delay: 1s;
-  transition-duration: 0.5s, 0.5s, 0.5s, 0.5s;
 `;
 
 function Versus() {
