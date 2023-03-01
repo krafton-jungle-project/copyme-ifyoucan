22c5a5fdb358d016f311d95f94eb792d8a97da03
jungle-taeuk <gotlou8317@gmail.com>
2023-03-01 14:12:00 +0900

    feat: 확대 및 grading 타이밍 조정(game atom 변경)

--- packages/client/src/components/inGame/GameEventHandler.tsx
+++ packages/client/src/components/inGame/GameEventHandler.tsx
@@ -64,20 +64,47 @@ const GameEventHandler = () => {
         CameraClick.play();
 
         if (stage === 'offend') {
+          // 라운드 전환
           setTimeout(() => {
             setGame((prev) => ({ ...prev, stage: GameStage.DEFEND }));
           }, 2500);
         }
 
         if (stage === 'defend') {
+          // 공수 비교 이펙트(캡쳐된 공격자, 수비자 사진 확대 비교 및 grading)
           setTimeout(() => {
-            setGame((prev) => ({
-              ...prev,
-              stage: Number.isInteger(prev.round + 0.5) ? GameStage.ROUND : GameStage.OFFEND,
-              round: prev.round + 0.5,
-              user: { ...prev.user, isOffender: !prev.user.isOffender }, // 공수전환
-            }));
-          }, 4000);
+            // 카운트다운 0(캡쳐) 이후 1초 지났을 때 캡쳐된 사진 크기 키우고
+            setGame((prev) => ({ ...prev, isCaptured: true }));
+
+            setTimeout(() => {
+              // 1초 뒤 수비자 캡쳐 사진에 grading 하고
+              setGame((prev) => ({
+                ...prev,
+                user: { ...prev.user, gradable: prev.user.isOffender ? false : true },
+                peer: { ...prev.peer, gradable: prev.user.isOffender ? true : false },
+              }));
+
+              // 1초 뒤 사진 다시 작게 하고, grading도 감추고
+              setTimeout(() => {
+                setGame((prev) => ({
+                  ...prev,
+                  isCaptured: false,
+                  user: { ...prev.user, gradable: false },
+                  peer: { ...prev.peer, gradable: false },
+                }));
+
+                // 0.5 라운드 증가(다음 라운드(+공수 전환) or 공수 전환)
+                setTimeout(() => {
+                  setGame((prev) => ({
+                    ...prev,
+                    stage: Number.isInteger(prev.round + 0.5) ? GameStage.ROUND : GameStage.OFFEND,
+                    round: prev.round + 0.5,
+                    user: { ...prev.user, isOffender: !prev.user.isOffender }, // 공수전환
+                  }));
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
         }
       }
     });
