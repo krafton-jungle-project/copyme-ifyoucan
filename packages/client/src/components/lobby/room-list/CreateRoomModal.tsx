511f273444de01d2cc678b1a0899b1c601e92c01
Choinbong <cho5048@dgu.ac.kr>
2023-03-04 16:19:06 +0900

    feat: 아이템 라운드 선택 틀 및 소켓 전송 구현

--- packages/client/src/components/lobby/room-list/CreateRoomModal.tsx
+++ packages/client/src/components/lobby/room-list/CreateRoomModal.tsx
@@ -2,6 +2,7 @@ import { useAtom, useSetAtom } from 'jotai';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled, { css, keyframes } from 'styled-components';
+import { ItemType } from '../../../app/game';
 import { createRoomModalAtom, fadeOutAtom, roomInfoAtom } from '../../../app/room';
 import { useClientSocket } from '../../../module/client-socket';
 
@@ -50,11 +51,11 @@ const Modal2 = styled.div<{ isOpened: boolean; isVisible: boolean }>`
   border-radius: 15px;
   position: absolute;
   transform: translate(-50%);
-  width: 30vw;
-  height: 35vh;
+  width: 35vw;
+  height: 55vh;
   left: 50%;
-  max-height: 250px;
-  max-width: 500px;
+  max-height: 400px;
+  max-width: 600px;
   background-color: #01000d;
   z-index: 999;
   display: flex;
@@ -136,6 +137,49 @@ const Button = styled.button`
   }
 `;
 
+const ModeDiv = styled.div`
+  border: 2px solid #fff;
+  border-radius: 10px;
+
+  padding: 2% 0 2% 5%;
+  height: 40%;
+  width: 85%;
+  display: flex;
+  /* flex-direction: column; */
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const RoundWrapper = styled.div`
+  width: 27%;
+  height: 100%;
+`;
+
+const RoundDiv = styled.div`
+  height: 33%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  font-size: 1.2rem;
+`;
+
+const SelectionWrapper = styled.div`
+  width: 73%;
+  height: 100%;
+`;
+
+const SelectionDiv = styled.div`
+  /* position: absolute; */
+  font-size: 1.2rem;
+  height: 33%;
+  display: flex;
+  align-items: center;
+`;
+
+const SelSpan = styled.span`
+  cursor: pointer;
+`;
+
 function CreateRoomModal() {
   const { socket } = useClientSocket();
   const setRoomInfo = useSetAtom(roomInfoAtom);
@@ -143,6 +187,11 @@ function CreateRoomModal() {
   const [roomName, setRoomName] = useState<string>('');
   const [open, setOpen] = useAtom(createRoomModalAtom);
   const [visible, setVisible] = useAtom(fadeOutAtom);
+  const [itemType, setItemType] = useState({
+    round1: ItemType.NORMAL,
+    round2: ItemType.NORMAL,
+    round3: ItemType.NORMAL,
+  });
 
   const joinRoom = () => {
     socket.on('new_room', (roomId: string) => {
@@ -150,6 +199,7 @@ function CreateRoomModal() {
       setRoomInfo(() => ({
         roomId,
         host: true,
+        itemType,
       }));
       navigate('/room', { replace: true });
     });
@@ -168,6 +218,26 @@ function CreateRoomModal() {
     }, 300);
   };
 
+  const handleMode = (e: any) => {
+    let round: string = `round${e.target.lang}`;
+    switch (e.target.innerText) {
+      case '노말':
+        setItemType((prev) => ({ ...prev, [round]: ItemType.NORMAL }));
+        break;
+      case '블러':
+        setItemType((prev) => ({ ...prev, [round]: ItemType.BLUR }));
+        break;
+      case '축소':
+        setItemType((prev) => ({ ...prev, [round]: ItemType.SIZEDOWN }));
+        break;
+      case '회전':
+        setItemType((prev) => ({ ...prev, [round]: ItemType.ROTATE }));
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <>
       {open && <ModalBackground isOpened={open} onClick={lazyClose} isVisible={visible} />}
@@ -182,6 +252,67 @@ function CreateRoomModal() {
           }}
           value={roomName}
         />
+        <Title>모드 선택</Title>
+        <ModeDiv>
+          <RoundWrapper>
+            <RoundDiv>Round 1 :</RoundDiv>
+            <RoundDiv>Round 2 :</RoundDiv>
+            <RoundDiv>Round 3 :</RoundDiv>
+          </RoundWrapper>
+          <SelectionWrapper>
+            <SelectionDiv>
+              <SelSpan lang="1" onClick={handleMode}>
+                노말
+              </SelSpan>
+              <span>　|　</span>
+              <SelSpan lang="1" onClick={handleMode}>
+                블러
+              </SelSpan>
+              <span>　|　</span>
+              <SelSpan lang="1" onClick={handleMode}>
+                축소
+              </SelSpan>
+              <span>　|　</span>
+              <SelSpan lang="1" onClick={handleMode}>
+                회전
+              </SelSpan>
+            </SelectionDiv>
+            <SelectionDiv>
+              <SelSpan lang="2" onClick={handleMode}>
+                노말
+              </SelSpan>
+              <span>　|　</span>
+              <SelSpan lang="2" onClick={handleMode}>
+                블러
+              </SelSpan>
+              <span>　|　</span>
+              <SelSpan lang="2" onClick={handleMode}>
+                축소
+              </SelSpan>
+              <span>　|　</span>
+              <SelSpan lang="2" onClick={handleMode}>
+                회전
+              </SelSpan>
+            </SelectionDiv>
+            <SelectionDiv>
+              <SelSpan lang="3" onClick={handleMode}>
+                노말
+              </SelSpan>
+              <span>　|　</span>
+              <SelSpan lang="3" onClick={handleMode}>
+                블러
+              </SelSpan>
+              <span>　|　</span>
+              <SelSpan lang="3" onClick={handleMode}>
+                축소
+              </SelSpan>
+              <span>　|　</span>
+              <SelSpan lang="3" onClick={handleMode}>
+                회전
+              </SelSpan>
+            </SelectionDiv>
+          </SelectionWrapper>
+        </ModeDiv>
         <Button onClick={handleOk}>생성하기</Button>
       </Modal2>
     </>
