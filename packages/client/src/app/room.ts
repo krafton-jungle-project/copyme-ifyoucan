fb005f78ef03853118f7e6fad249430339a054af
Choinbong <cho5048@dgu.ac.kr>
2023-03-04 16:17:48 +0900

    feat: added initialized itemType in roomState

--- packages/client/src/app/room.ts
+++ packages/client/src/app/room.ts
@@ -2,6 +2,7 @@ import { atomWithReset } from 'jotai/utils';
 import { atom, useAtomValue, useSetAtom } from 'jotai';
 import type { Room, Rooms } from 'project-types';
 import { useMemo } from 'react';
+import { ItemType } from './game';
 
 const roomsAtom = atom<Rooms>({});
 
@@ -23,11 +24,21 @@ const useRoomAtom = () => {
 interface RoomState {
   roomId: string;
   host: boolean;
+  itemType: {
+    round1: number;
+    round2: number;
+    round3: number;
+  };
 }
 
 const RoomInitialState: RoomState = {
   roomId: '',
   host: false,
+  itemType: {
+    round1: ItemType.NORMAL,
+    round2: ItemType.NORMAL,
+    round3: ItemType.NORMAL,
+  },
 };
 
 const roomInfoAtom = atomWithReset(RoomInitialState);
